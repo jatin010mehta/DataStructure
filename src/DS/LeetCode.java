c8b0656354caef2d844ac13a49056405c2bf8c94
Jatin Mehta <jatin14mehta@gmail.com>
2020-11-08 18:37:15 -0800

    longestIncreasingPath

--- src/DS/LeetCode.java
+++ src/DS/LeetCode.java
@@ -35,4 +35,32 @@ public class LeetCode {
         grid[i][j]=temp;
 
     }
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix.length==0)
+            return 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (dp[i][j]==0){
+                    max=Math.max(max,dfsForLongestIncreasing(matrix,i,j,dp,Integer.MIN_VALUE));
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfsForLongestIncreasing(int[][] matrix, int i, int j, int[][] dp, int pre) {
+        if (i<0||j<0||i>= matrix.length||j>= matrix[0].length)return 0;
+        else if (pre>=matrix[i][j]) return 0;
+        else if (dp[i][j]!=0)return dp[i][j];
+        int path1 = dfsForLongestIncreasing(matrix,i+1,j,dp,matrix[i][j]);
+        int path2 = dfsForLongestIncreasing(matrix,i-1,j,dp,matrix[i][j]);
+        int path3 = dfsForLongestIncreasing(matrix,i,j+1,dp,matrix[i][j]);
+        int path4 = dfsForLongestIncreasing(matrix,i,j-1,dp,matrix[i][j]);
+        dp[i][j] =1+ Math.max(Math.max(path1,path2),Math.max(path3,path4));
+        return dp[i][j];
+
+    }
 }
