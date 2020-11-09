b45299932ed46b18fd3aa0007de41b23f79e9a8d
Jatin Mehta <jatin14mehta@gmail.com>
2020-11-08 16:47:01 -0800

    uniquePath

--- src/DS/LeetCode.java
+++ src/DS/LeetCode.java
@@ -1,4 +1,38 @@
 package DS;
 
 public class LeetCode {
+    int totalPath = 0;
+    public int uniquePathsIII(int[][] grid) {
+        int totalZero = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]==0)
+                    totalZero++;
+            }
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]==1)
+                    dfsForUniquePath(grid,i,j,totalZero);
+            }
+        }
+        return totalPath;
+    }
+
+    private void dfsForUniquePath(int[][] grid, int i, int j, int totalZero) {
+        if (i<0||j<0||i>= grid.length||j>=grid[0].length||grid[i][j]==-1||(grid[i][j]==2&&totalZero!=0)||grid[i][j]==99)
+            return;
+        if (grid[i][j]==2&&totalZero==0)
+            totalPath++;
+        if (grid[i][j]==0)
+            totalZero--;
+        int temp = grid[i][j];
+        grid[i][j]=99;
+        dfsForUniquePath(grid,i+1,j,totalZero);
+        dfsForUniquePath(grid,i-1,j,totalZero);
+        dfsForUniquePath(grid,i,j+1,totalZero);
+        dfsForUniquePath(grid,i,j-1,totalZero);
+        grid[i][j]=temp;
+
+    }
 }
--- src/DS/Main.java
+++ src/DS/Main.java
@@ -1,8 +1,17 @@
 package DS;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Amazon amazon = new Amazon();
+        Scanner input = new Scanner(System.in);
+        int num1= input.nextInt();
+        String string = input.nextLine();
+        System.out.print(num1);
+        System.out.println(string);
+
+
 
     }
 }
